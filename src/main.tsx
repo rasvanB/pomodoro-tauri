9e5b814a501dfecac7605fb7968c75cc67d50d7d
rasvanB <not.obelix@gmail.com>
2022-09-09 00:50:13 +0300

    added custom title bar

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style.css";
 import "./App.css";
+import { appWindow, LogicalSize } from "@tauri-apps/api/window";
+
+appWindow.setResizable(false);
+appWindow.setSize(new LogicalSize(500, 600));
+appWindow.setDecorations(false);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
