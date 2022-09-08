1d725ceade94bc5e71e31386542646d72c2b8514
rasvanB <not.obelix@gmail.com>
2022-09-09 00:56:27 +0300

    created components folder and custom titlebar component file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,4 @@
-import { appWindow } from "@tauri-apps/api/window";
-import { useEffect } from "react";
-
 const App = () => {
-  useEffect(() => {
-    const minimizeButton = document.getElementById("titlebar-minimize");
-    const closeButton = document.getElementById("titlebar-close");
-    if (minimizeButton)
-      minimizeButton.addEventListener("click", () => appWindow.minimize());
-    if (closeButton)
-      closeButton.addEventListener("click", () => appWindow.close());
-  }, []);
   return <div className="text-red-500">COOLBRO</div>;
 };
 
