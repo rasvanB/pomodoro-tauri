c763a7a98731f51aaed4b8cc1f6e983eaf34181b
rasvanB <not.obelix@gmail.com>
2022-09-09 22:29:58 +0300

    window is now visible only after App component is mounted

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { appWindow } from "@tauri-apps/api/window";
+import { useEffect } from "react";
 import Titlebar from "./components/titlebar.component";
 
 const App = () => {
+  // make window visible on component mount - prevents white flash
+  useEffect(() => {
+    appWindow.show();
+  }, []);
+
   return (
     <div className="text-red-500">
       <Titlebar />
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style.css";
 import "./App.css";
-import { appWindow, LogicalSize } from "@tauri-apps/api/window";
-
-appWindow.setResizable(false);
-appWindow.setDecorations(false);
-appWindow.setSize(new LogicalSize(500, 600));
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
