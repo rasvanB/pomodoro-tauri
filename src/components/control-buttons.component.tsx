053be8072ddb69e9d535316929dc4ccea8bcf33b
rasvanB <not.obelix@gmail.com>
2022-09-11 00:22:31 +0300

    changed pause icon

--- src/components/control-buttons.component.tsx
+++ src/components/control-buttons.component.tsx
@@ -7,7 +7,7 @@ const ControlButtons = () => {
         <Icon icon="codicon:debug-restart" className="text-[25px]" />
       </button>
       <button className="bg-[#7509FF] w-[60px] h-[60px] flex justify-center items-center rounded-md drop-shadow-[0_0_10px_rgba(129,31,255,0.4)]">
-        <Icon icon="fe:pause" className="text-[56px]"></Icon>
+        <Icon icon="heroicons:pause-20-solid" className="text-[45px]"></Icon>
       </button>
       <button className="bg-[#181828] w-[55px] h-[55px] rounded-full flex justify-center items-center">
         <Icon icon="bi:stop-fill" className="text-[32px]"></Icon>
