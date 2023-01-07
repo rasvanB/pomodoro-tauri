c58439251b3c45e8a31e69d11800cba2cc655659
rasvanB <not.obelix@gmail.com>
2023-01-07 02:21:39 +0200

    added the settings menu to titlebar

--- src/components/titlebar.tsx
+++ src/components/titlebar.tsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
+import SettingsMenu from "./settings-menu";
 import TitleBarButton from "./titlebar-button";
 import TitleBarButtons from "./titlebar-buttons";
 
 const Titlebar = () => {
+  const [showSettings, setShowSettings] = useState(true);
+  const toggleSettings = () => setShowSettings((prev) => !prev);
+
   return (
-    <div
-      data-tauri-drag-region
-      className="w-[100vw] h-[30px] bg-[#11111D] flex"
-    >
-      <TitleBarButton icon="clarity:menu-line" onClick={() => {}} />
-      <TitleBarButtons />
+    <div className="overflow-hidden">
+      <div
+        data-tauri-drag-region
+        className="w-[100vw] h-[30px] bg-[#11111D] flex z-[999] relative"
+      >
+        <TitleBarButton icon="clarity:menu-line" onClick={toggleSettings} />
+        <TitleBarButtons />
+      </div>
+      <SettingsMenu show={showSettings} />
     </div>
   );
 };
