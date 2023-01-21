b1ee4a3084398d2376b2c8fbb8a43f7c9ff1bcab
rasvanB <not.obelix@gmail.com>
2023-01-21 21:37:52 +0200

    created atoms for different timer actions

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -16,4 +16,30 @@ export const timerAtom = atom({
   isRunning: false,
 });
 
+export const incrementTimerAtom = atom(null, (get, set) => {
+  const { passedSeconds, currentRound, isRunning } = get(timerAtom);
+  set(timerAtom, {
+    passedSeconds: passedSeconds + 1,
+    currentRound,
+    isRunning,
+  });
+});
+
+export const resetTimerAtom = atom(null, (get, set) => {
+  set(timerAtom, {
+    passedSeconds: 0,
+    currentRound: 1,
+    isRunning: false,
+  });
+});
+
+export const incrementRoundAtom = atom(null, (get, set) => {
+  const prev = get(timerAtom);
+  set(timerAtom, {
+    currentRound: prev.currentRound + 1,
+    passedSeconds: 0,
+    isRunning: false,
+  });
+});
+
 export const settingsAtom = atomWithStorage("settings", initialSettings);
