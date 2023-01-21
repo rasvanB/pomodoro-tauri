0f41a4c8afd9dd9a17b8a83a42d6f246812077d5
rasvanB <not.obelix@gmail.com>
2023-01-21 21:38:19 +0200

    changed prop name

--- src/components/switch-button.tsx
+++ src/components/switch-button.tsx
@@ -1,24 +1,24 @@
 import { CSSTransition } from "react-transition-group";
 
 type SwitchButtonProps = {
-  isOn: boolean;
+  value: boolean;
   onToggle: () => void;
   label?: string;
 };
 
-function SwitchButton({ isOn, onToggle, label }: SwitchButtonProps) {
+function SwitchButton({ value, onToggle, label }: SwitchButtonProps) {
   return (
     <div className="flex justify-center items-center mt-2">
       {label && <span className="mr-4 font-inter">{label}</span>}
       <button
-        className={`flex w-[48px] rounded-full focus:outline-none transition-colors ${
-          isOn ? "bg-purple-500" : "bg-[#1a1a2c]"
+        className={`flex w-[48.5px] rounded-full focus:outline-none transition-colors ${
+          value ? "bg-purple-500" : "bg-[#1a1a2c]"
         }`}
         onClick={onToggle}
       >
         <div
           className={`${
-            isOn ? "translate-x-full" : "translate-x-0"
+            value ? "translate-x-full" : "translate-x-0"
           } inline-block h-6 w-6 rounded-full bg-white shadow transform`}
         />
       </button>
