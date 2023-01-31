e07ac4ea964d4e8a4d30e297929c08d54b97fdfc
rasvanB <not.obelix@gmail.com>
2023-01-31 19:39:01 +0200

    focus styles for control buttons

--- src/components/control-buttons.tsx
+++ src/components/control-buttons.tsx
@@ -18,7 +18,7 @@ const ControlButtons = () => {
     <div className="text-center flex gap-5 text-white items-center justify-center mt-7">
       <SideButton type="reset" onClick={resetTimer} />
       <button
-        className="z-0 w-[60px] h-[60px] flex justify-center items-center rounded-md dark:bg-[#199221] dark:hover:bg-[#1ea827] dark:drop-shadow-[0_0_10px_rgba(31,255,56,0.3)] dark:hover:drop-shadow-[0_0_20px_rgba(31,255,56,0.3)] bg-[#7509FF] hover:bg-[#7d28ec] drop-shadow-[0_0_10px_rgba(129,31,255,0.4)] hover:drop-shadow-[0_0_20px_rgba(129,31,255,0.4)]"
+        className="z-0 w-[60px] h-[60px] flex justify-center items-center rounded-md dark:bg-[#199221] dark:hover:bg-[#1ea827] dark:drop-shadow-[0_0_10px_rgba(31,255,56,0.3)] dark:hover:drop-shadow-[0_0_20px_rgba(31,255,56,0.3)] bg-[#7509FF] hover:bg-[#7d28ec] drop-shadow-[0_0_10px_rgba(129,31,255,0.4)] hover:drop-shadow-[0_0_20px_rgba(129,31,255,0.4)] focus:ring-2 focus:ring-offset-1"
         onClick={toggleTimer}
         style={{
           transition: "all 0.3s ease-in-out",
